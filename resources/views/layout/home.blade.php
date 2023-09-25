00cf3e82b5a96011294d17d052e4a8fa4ce34daa
Renato Galvão <renatopgalvao@gmail.com>
2023-09-25 18:52:36 -0300

    Início da criação do layout e estilização

---
+++ resources/views/layout/home.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ToDo App</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/assets/css/style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="sidebar">
+            Logo
+        </div>
+        <div class="content">
+            <nav>
+                <a href="#" class="btn btn-primary">Criar Tarefa</a>
+            </nav>
+            <div class="main">
+                <section class="graph">
+                    Gráfico
+                </section>
+                <section class="list">
+                    Lista de tarefas
+                </section>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
